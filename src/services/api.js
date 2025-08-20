63c007bae745e33e6d3d6256d4940f6027945e8d
Jay <156161917+sonwanejay@users.noreply.github.com>
2025-08-20 15:34:15 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = "cd55a4dd28973e0aabd45169af86a9c7";
-const BASE_URL = "http://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
